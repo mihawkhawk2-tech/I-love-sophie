eaf7d69012169dd22804fd6350fb0303ce372ee5
mihawkhawk2-tech <mihawkhawk2@gmail.com>
2026-01-20 20:52:25 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-document.getElementById("myButton").addEventListener("click", function () {
-  alert("You clicked the button ❤️");
-});
+const button = document.getElementById("loveButton");
+const message = document.getElementById("secretMessage");
 
+button.addEventListener("click", function () {
+  message.classList.toggle("hidden");
+  button.textContent = "I Love You ❤️";
+});
